bf58f7d035feea2956b06b461a04c1f792c6d495
Obumneme Eziamaka <69645610+Obumezi@users.noreply.github.com>
2022-07-04 14:56:26 +0000

    Completed assignment 1 & 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,25 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+//first we set the initial counter to 0
+let total = 0;
+
+  //for every number in the array of numbers
+  for(let number of arrayOfNumbers){
+
+    //do this, add the value to the total add assign it to the number
+    total += number;
+  }
+  //return the total counter here this ends the function
+  return total
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+assignment.sumOfNumbers = sumOfNumbers;
+
+
 
 /**
  * Challenge - 2
@@ -28,9 +43,22 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  //set initial count
+  let count = 0
+
+  //loop through the array and @ every position of i
+  //check that when it is divided by 2 its remainder is 0
+  for (let i = 0; i < arrayOfNumbers.length; i++){
+    if (arrayOfNumbers[i] % 2 === 0){
+
+      //increase the count
+      count++
+    }
+  }
+  //end program
+  return count
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
